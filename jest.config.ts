f2c6ea02b7fdcd198ceda7667f1cf0747ebc51e5
olenaweb <olena.nevzorova@gmail.com>
2024-07-14 23:31:37 +0300

    feat: install test

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': '<rootDir>/test/__mocks__/fileMock.js',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js'
-  }
+    "\\.(css|less|scss|sass)$": "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+  },
 };
