a7ba2f409f5432bb7903b73d65de333370649e8a
olenaweb <olena.nevzorova@gmail.com>
2024-07-25 12:14:31 +0300

    fix: changed some styles, install @types/react-redux

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -3,7 +3,6 @@ import { TypedUseSelectorHook, useSelector } from "react-redux";
 import { RootState } from "./Store";
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-
 // import { useDispatch } from 'react-redux';
 // import type { AppDispatch } from './Store';
 // export const useAppDispatch: () => AppDispatch = useDispatch;
