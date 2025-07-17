1cfc4a8f876a07f33de4415284873be522df148a
olenaweb <olena.nevzorova@gmail.com>
2025-07-17 16:38:27 +0300

    feat: modify configurations jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -9,4 +9,25 @@ export default {
     "\\.(jpg|jpeg|png|gif|svg|woff|woff2|ttf|eot)$": "<rootDir>/test/__mocks__/fileMock.js",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/index.{js,jsx,ts,tsx}',
+    '!src/setupTests.{js,ts}',
+    '!src/**/*.d.ts',
+    "!<rootDir>/node_modules/",
+    "!<rootDir>/dist/",
+    "!<rootDir>/"
+  ],
+  coverageThreshold: {
+    "global": {
+      "statements": 80,
+      "branches": 50,
+      "functions": 50,
+      "lines": 50
+    }
+  },
+  coverageReporters: [
+    "text"
+  ]
 };
