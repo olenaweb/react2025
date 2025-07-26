0d495eb49edefd87ff40ead9aeadcb6d50111587
olenaweb <olena.nevzorova@gmail.com>
2025-07-26 22:01:20 +0300

    feat: add CardList

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -12,9 +12,7 @@ interface Props {
 
 export default function Pagination(props: Props) {
   const { currentPage, updateCurrentPage, nextPage, lastPage } = props;
-  // if (!currentPage || !updateCurrentPage) {
-  //   throw new Error("Pagination component requires currentPage and updateCurrentPage props");
-  // }
+
   const currentPageValue = Number(currentPage) ? currentPage : "1";
   const [page, setPage] = useState<string>(currentPageValue);
   const nextPageValue = nextPage ? Number(nextPage) : null;
--- src/containers/CardList.tsx
+++ src/containers/CardList.tsx
@@ -1,15 +1,14 @@
-import { Component } from "react";
 import { Card } from "../components/Card";
 import { AllCharacter } from "../types/types";
 
-export class CardList extends Component<AllCharacter> {
-  render() {
-    return (
-      <ul className="cards">
-        {this.props.results.map((item) => (
-          <Card key={item.id} {...item} />
-        ))}
-      </ul>
-    );
-  }
-}
+import React from "react";
+
+export const CardList: React.FC<AllCharacter> = ({ results }) => {
+  return (
+    <ul className="cards">
+      {results.map((item) => (
+        <Card key={item.id} {...item} />
+      ))}
+    </ul>
+  );
+};
