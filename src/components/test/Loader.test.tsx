caf3139e3b53d0d910da02c7fbd5dfc23b6faa64
olenaweb <olena.nevzorova@gmail.com>
2025-08-15 02:06:59 +0300

    init: setup project

--- src/components/test/Loader.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Loader from "../Loader";
-
-test("renders loading message", () => {
-  render(<Loader />);
-  const loadingMessage = screen.getByText(/Loading.../i);
-  expect(loadingMessage).toBeInTheDocument();
-});
